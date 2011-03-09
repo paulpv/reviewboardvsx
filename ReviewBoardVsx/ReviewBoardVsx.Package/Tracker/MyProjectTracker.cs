c1b510e20b377e43b1ab1fa52a27c7edc149047c
paul.peavyhouse <paul.peavyhouse@gmail.com>
2011-03-09 07:34:31 +0000

    MUCH cleaner code! Baby step progress towards handling all Project/Solution item renames. I catch Project and Solution file renames, but I still don't do anything with them yet. This check-in breaks a few things, but its late and I probably won't fix until tomorrow.

---
+++ ReviewBoardVsx/ReviewBoardVsx.Package/Tracker/MyProjectTracker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ReviewBoardVsx.Package.Tracker
+{
+    /// <summary>
+    /// </summary>
+    class MyProjectTracker : ProjectDocumentsListener
+    {
+        private MySolutionTracker solutionTracker;
+
+        public MyProjectTracker(MySolutionTracker solutionTracker)
+            : base(solutionTracker.ServiceProvider)
+        {
+            this.solutionTracker = solutionTracker;
+
+            this.FileAdded += MyProjectTracker_FileAdded;
+            this.FileRenamed += MyProjectTracker_FileRenamed;
+            this.FileRemoved += MyProjectTracker_FileRemoved;
+        }
+
+        void MyProjectTracker_FileAdded(object sender, ProjectDocumentsListener.ProjectItemAddRemoveEventArgs e)
+        {
+            //solutionTracker.AddFilePathIfChanged(e.Path);
+        }
+
+        void MyProjectTracker_FileRenamed(object sender, ProjectDocumentsListener.ProjectItemRenameEventArgs e)
+        {
+        }
+
+        void MyProjectTracker_FileRemoved(object sender, ProjectDocumentsListener.ProjectItemAddRemoveEventArgs e)
+        {
+        }
+    }
+}
--- ReviewBoardVsx/ReviewBoardVsx.Package/Tracker/ProjectDocumentsListener.cs
+++ ReviewBoardVsx/ReviewBoardVsx.Package/Tracker/ProjectDocumentsListener.cs
@@ -8,12 +8,51 @@ using ShellConstants = Microsoft.VisualStudio.Shell.Interop.Constants;
 namespace ReviewBoardVsx.Package.Tracker
 {
     /// <summary>
-    /// Most of this class was shamelessly copied from:
+    /// This class keeps track of files and directories being added, renamed [moved?], or removed from a Project.
+    /// 
+    /// Ideas came from Ankh ProjectTracker and:
     /// http://www.java2s.com/Open-Source/CSharp/Development/StyleCop/Microsoft/VisualStudio/Package/ProjectDocumentsListener.cs.htm
+    /// http://www.java2s.com/Open-Source/CSharp/Development/StyleCop/Microsoft/VisualStudio/Shell/Flavor/Project.cs.htm
+    /// http://www.java2s.com/Open-Source/CSharp/Development/StyleCop/Microsoft/VisualStudio/Shell/Flavor/ProjectDocumentsChangeEventsArgs.cs.htm
     /// </summary>
     [CLSCompliant(false)]
     public abstract class ProjectDocumentsListener : IVsTrackProjectDocumentsEvents2, IDisposable
     {
+        public class ProjectItemAddRemoveEventArgs : EventArgs
+        {
+            public IVsProject Project { get; private set; }
+            public String Path { get; private set; }
+
+            public ProjectItemAddRemoveEventArgs(IVsProject project, string path)
+            {
+                Project = project;
+                Path = path;
+            }
+        }
+
+        public class ProjectItemRenameEventArgs : EventArgs
+        {
+            public IVsProject Project { get; private set; }
+            public String PathOld { get; private set; }
+            public String PathNew { get; private set; }
+
+            public ProjectItemRenameEventArgs(IVsProject project, string pathOld, string pathNew)
+            {
+                Project = project;
+                PathOld = pathOld;
+                PathNew = pathNew;
+            }
+        }
+
+        //public delegate void EventHandler<ProjectDocumentsChangeEventArgs>(object sender, ProjectDocumentsChangeEventArgs e);
+
+        public event EventHandler<ProjectItemAddRemoveEventArgs> FileAdded;
+        public event EventHandler<ProjectItemRenameEventArgs> FileRenamed;
+        public event EventHandler<ProjectItemAddRemoveEventArgs> FileRemoved;
+        public event EventHandler<ProjectItemAddRemoveEventArgs> DirectoryAdded;
+        public event EventHandler<ProjectItemRenameEventArgs> DirectoryRenamed;
+        public event EventHandler<ProjectItemAddRemoveEventArgs> DirectoryRemoved;
+
         public IServiceProvider ServiceProvider { get; private set; }
 
         private IVsTrackProjectDocuments2 projectDocumentTracker2;
@@ -34,23 +73,13 @@ namespace ReviewBoardVsx.Package.Tracker
             }
         }
 
-        #region IVsTrackProjectDocumentsEvents2 Members
-
-        public abstract int OnAfterAddDirectoriesEx(int cProjects, int cDirectories, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSADDDIRECTORYFLAGS[] rgFlags);
-        public abstract int OnAfterAddFilesEx(int cProjects, int cFiles, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSADDFILEFLAGS[] rgFlags);
-        public abstract int OnAfterRemoveDirectories(int cProjects, int cDirectories, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSREMOVEDIRECTORYFLAGS[] rgFlags);
-        public abstract int OnAfterRemoveFiles(int cProjects, int cFiles, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSREMOVEFILEFLAGS[] rgFlags);
-        public abstract int OnAfterRenameDirectories(int cProjects, int cDirs, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgszMkOldNames, string[] rgszMkNewNames, VSRENAMEDIRECTORYFLAGS[] rgFlags);
-        public abstract int OnAfterRenameFiles(int cProjects, int cFiles, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgszMkOldNames, string[] rgszMkNewNames, VSRENAMEFILEFLAGS[] rgFlags);
-        public abstract int OnAfterSccStatusChanged(int cProjects, int cFiles, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, uint[] rgdwSccStatus);
-        public abstract int OnQueryAddDirectories(IVsProject pProject, int cDirectories, string[] rgpszMkDocuments, VSQUERYADDDIRECTORYFLAGS[] rgFlags, VSQUERYADDDIRECTORYRESULTS[] pSummaryResult, VSQUERYADDDIRECTORYRESULTS[] rgResults);
-        public abstract int OnQueryAddFiles(IVsProject pProject, int cFiles, string[] rgpszMkDocuments, VSQUERYADDFILEFLAGS[] rgFlags, VSQUERYADDFILERESULTS[] pSummaryResult, VSQUERYADDFILERESULTS[] rgResults);
-        public abstract int OnQueryRemoveDirectories(IVsProject pProject, int cDirectories, string[] rgpszMkDocuments, VSQUERYREMOVEDIRECTORYFLAGS[] rgFlags, VSQUERYREMOVEDIRECTORYRESULTS[] pSummaryResult, VSQUERYREMOVEDIRECTORYRESULTS[] rgResults);
-        public abstract int OnQueryRemoveFiles(IVsProject pProject, int cFiles, string[] rgpszMkDocuments, VSQUERYREMOVEFILEFLAGS[] rgFlags, VSQUERYREMOVEFILERESULTS[] pSummaryResult, VSQUERYREMOVEFILERESULTS[] rgResults);
-        public abstract int OnQueryRenameDirectories(IVsProject pProject, int cDirs, string[] rgszMkOldNames, string[] rgszMkNewNames, VSQUERYRENAMEDIRECTORYFLAGS[] rgFlags, VSQUERYRENAMEDIRECTORYRESULTS[] pSummaryResult, VSQUERYRENAMEDIRECTORYRESULTS[] rgResults);
-        public abstract int OnQueryRenameFiles(IVsProject pProject, int cFiles, string[] rgszMkOldNames, string[] rgszMkNewNames, VSQUERYRENAMEFILEFLAGS[] rgFlags, VSQUERYRENAMEFILERESULTS[] pSummaryResult, VSQUERYRENAMEFILERESULTS[] rgResults);
-
-        #endregion
+        public void Initialize()
+        {
+            if (projectDocumentTracker2 != null && eventsCookie == (uint)ShellConstants.VSCOOKIE_NIL)
+            {
+                ErrorHandler.ThrowOnFailure(projectDocumentTracker2.AdviseTrackProjectDocumentsEvents(this, out eventsCookie));
+            }
+        }
 
         public void Dispose()
         {
@@ -74,11 +103,296 @@ namespace ReviewBoardVsx.Package.Tracker
             }
         }
 
-        public void Initialize()
+        #region IVsTrackProjectDocumentsEvents2 methods
+
+        public int OnQueryAddFiles(IVsProject pProject, int cFiles, string[] rgpszMkDocuments, VSQUERYADDFILEFLAGS[] rgFlags, VSQUERYADDFILERESULTS[] pSummaryResult, VSQUERYADDFILERESULTS[] rgResults)
         {
-            if (projectDocumentTracker2 != null && eventsCookie == (uint)ShellConstants.VSCOOKIE_NIL)
+            try
             {
-                ErrorHandler.ThrowOnFailure(projectDocumentTracker2.AdviseTrackProjectDocumentsEvents(this, out eventsCookie));
+                MyLog.DebugEnter(this, "OnQueryAddFiles");
+                return VSConstants.S_OK; // ignore
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnQueryAddFiles");
+            }
+        }
+
+        public int OnAfterAddFilesEx(int cProjects, int cFiles, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSADDFILEFLAGS[] rgFlags)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnAfterAddFilesEx");
+                GenerateAddRemoveEvents(rgpProjects, rgFirstIndices, rgpszMkDocuments, FileAdded);
+                return VSConstants.S_OK;
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnAfterAddFilesEx");
+            }
+        }
+
+        public int OnQueryRenameFiles(IVsProject pProject, int cFiles, string[] rgszMkOldNames, string[] rgszMkNewNames, VSQUERYRENAMEFILEFLAGS[] rgFlags, VSQUERYRENAMEFILERESULTS[] pSummaryResult, VSQUERYRENAMEFILERESULTS[] rgResults)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnQueryRenameFiles");
+                return VSConstants.S_OK; // ignore
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnQueryRenameFiles");
+            }
+        }
+
+        public int OnAfterRenameFiles(int cProjects, int cFiles, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgszMkOldNames, string[] rgszMkNewNames, VSRENAMEFILEFLAGS[] rgFlags)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnAfterRenameFiles");
+                GenerateRenameEvents(rgpProjects, rgFirstIndices, rgszMkOldNames, rgszMkNewNames, FileRenamed);
+                return VSConstants.S_OK;
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnAfterRenameFiles");
+            }
+        }
+
+        public int OnQueryRemoveFiles(IVsProject pProject, int cFiles, string[] rgpszMkDocuments, VSQUERYREMOVEFILEFLAGS[] rgFlags, VSQUERYREMOVEFILERESULTS[] pSummaryResult, VSQUERYREMOVEFILERESULTS[] rgResults)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnQueryRemoveFiles");
+                return VSConstants.S_OK; // ignore
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnQueryRemoveFiles");
+            }
+        }
+
+        public int OnAfterRemoveFiles(int cProjects, int cFiles, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSREMOVEFILEFLAGS[] rgFlags)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnAfterRemoveFiles");
+                GenerateAddRemoveEvents(rgpProjects, rgFirstIndices, rgpszMkDocuments, FileRemoved);
+                return VSConstants.S_OK;
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnAfterRemoveFiles");
+            }
+        }
+
+        public int OnQueryAddDirectories(IVsProject pProject, int cDirectories, string[] rgpszMkDocuments, VSQUERYADDDIRECTORYFLAGS[] rgFlags, VSQUERYADDDIRECTORYRESULTS[] pSummaryResult, VSQUERYADDDIRECTORYRESULTS[] rgResults)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnQueryAddDirectories");
+                return VSConstants.S_OK; // ignore
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnQueryAddDirectories");
+            }
+        }
+
+        public int OnAfterAddDirectoriesEx(int cProjects, int cDirectories, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSADDDIRECTORYFLAGS[] rgFlags)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnAfterAddDirectoriesEx");
+                GenerateAddRemoveEvents(rgpProjects, rgFirstIndices, rgpszMkDocuments, DirectoryAdded);
+                return VSConstants.S_OK;
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnAfterAddDirectoriesEx");
+            }
+        }
+
+        public int OnQueryRenameDirectories(IVsProject pProject, int cDirs, string[] rgszMkOldNames, string[] rgszMkNewNames, VSQUERYRENAMEDIRECTORYFLAGS[] rgFlags, VSQUERYRENAMEDIRECTORYRESULTS[] pSummaryResult, VSQUERYRENAMEDIRECTORYRESULTS[] rgResults)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnQueryRenameDirectories");
+                return VSConstants.S_OK; // ignore
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnQueryRenameDirectories");
+            }
+        }
+
+        public int OnAfterRenameDirectories(int cProjects, int cDirs, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgszMkOldNames, string[] rgszMkNewNames, VSRENAMEDIRECTORYFLAGS[] rgFlags)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnAfterRenameDirectories");
+                GenerateRenameEvents(rgpProjects, rgFirstIndices, rgszMkOldNames, rgszMkNewNames, DirectoryRenamed);
+                return VSConstants.S_OK;
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnAfterRenameDirectories");
+            }
+        }
+
+        public int OnQueryRemoveDirectories(IVsProject pProject, int cDirectories, string[] rgpszMkDocuments, VSQUERYREMOVEDIRECTORYFLAGS[] rgFlags, VSQUERYREMOVEDIRECTORYRESULTS[] pSummaryResult, VSQUERYREMOVEDIRECTORYRESULTS[] rgResults)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnQueryRemoveDirectories");
+                return VSConstants.S_OK; // ignore
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnQueryRemoveDirectories");
+            }
+        }
+
+        public int OnAfterRemoveDirectories(int cProjects, int cDirectories, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, VSREMOVEDIRECTORYFLAGS[] rgFlags)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnAfterRemoveDirectories");
+                GenerateAddRemoveEvents(rgpProjects, rgFirstIndices, rgpszMkDocuments, DirectoryRemoved);
+                return VSConstants.S_OK;
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnAfterRemoveDirectories");
+            }
+        }
+
+        public int OnAfterSccStatusChanged(int cProjects, int cFiles, IVsProject[] rgpProjects, int[] rgFirstIndices, string[] rgpszMkDocuments, uint[] rgdwSccStatus)
+        {
+            try
+            {
+                MyLog.DebugEnter(this, "OnAfterSccStatusChanged");
+                return VSConstants.S_OK; // ignore
+            }
+            finally
+            {
+                MyLog.DebugLeave(this, "OnAfterSccStatusChanged");
+            }
+        }
+
+        #endregion IVsTrackProjectDocumentsEvents2 methods
+
+        private void GenerateAddRemoveEvents(
+            IVsProject[] projects,
+            int[] firstPaths,
+            string[] paths,
+            EventHandler<ProjectItemAddRemoveEventArgs> eventToGenerate)
+        {
+            if (eventToGenerate == null)
+                return; // no event = nothing to do
+
+            if (projects == null || firstPaths == null || paths == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (projects.Length != firstPaths.Length)
+            {
+                throw new ArgumentException();
+            }
+
+            int cProjects = projects.Length;
+            int cPaths = paths.Length;
+
+            IVsProject project;
+            string origin;
+            string pathNew;
+
+            int iPath = 0;
+            for (int iProject = 0; (iProject < cProjects) && (iPath < cPaths); iProject++)
+            {
+                int iLastPathThisProject = (iProject < cProjects - 1) ? firstPaths[iProject + 1] : cPaths;
+
+                for (; iPath < iLastPathThisProject; iPath++)
+                {
+                    origin = null;
+                    pathNew = paths[iPath];// SvnTools.GetNormalizedFullPath(rgszMkNewNames[iFile]);
+
+                    // if project == null then Solution, else Project
+                    project = projects[iProject];
+
+                    try
+                    {
+                        eventToGenerate(this, new ProjectItemAddRemoveEventArgs(project, pathNew));
+                    }
+                    catch (Exception error)
+                    {
+                        Debug.Fail(error.Message);
+                    }
+                }
+            }
+        }
+
+        private void GenerateRenameEvents(
+            IVsProject[] projects,
+            int[] firstPaths,
+            string[] pathsOld,
+            string[] pathsNew,
+            EventHandler<ProjectItemRenameEventArgs> eventToGenerate)
+        {
+            if (eventToGenerate == null)
+                return; // no event = nothing to do
+
+            if (projects == null || firstPaths == null || pathsOld == null || pathsNew == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (projects.Length != firstPaths.Length)
+            {
+                throw new ArgumentException();
+            }
+
+            if (pathsOld.Length != pathsNew.Length)
+            {
+                throw new ArgumentException();
+            }
+
+            int cProjects = projects.Length;
+            int cPaths = pathsOld.Length;
+
+            // TODO: C++ projects do not send directory renames; but do send OnAfterRenameFile() events
+            //       for all files (one at a time). We should detect that case here and fix up this dirt!
+
+            IVsProject project;
+            string pathOld;
+            string pathNew;
+
+            int iPath = 0;
+            for (int iProject = 0; (iProject < cProjects) && (iPath < cPaths); iProject++)
+            {
+                int iLastPathThisProject = (iProject < cProjects - 1) ? firstPaths[iProject + 1] : cPaths;
+
+                for (; iPath < iLastPathThisProject; iPath++)
+                {
+                    pathOld = pathsOld[iPath];// SvnTools.GetNormalizedFullPath(pathsOld[iFile]);
+                    pathNew = pathsNew[iPath];// SvnTools.GetNormalizedFullPath(pathsNew[iFile]);
+
+                    if (pathOld == pathNew)
+                        continue;
+
+                    // if project == null then Solution, else Project
+                    project = projects[iProject];
+
+                    try
+                    {
+                        eventToGenerate(this, new ProjectItemRenameEventArgs(project, pathOld, pathNew));
+                    }
+                    catch (Exception error)
+                    {
+                        Debug.Fail(error.Message);
+                    }
+                }
             }
         }
     }
